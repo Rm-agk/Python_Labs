db4cdaed752b42f413b5e6770ef4d0f52ed7d219
Martins Richmond <79218307+Rm-agk@users.noreply.github.com>
2022-05-31 16:32:39 +0100

    Add files via upload

---
+++ 04names4.py
@@ -0,0 +1,17 @@
+import sys
+fullname = sys.argv[1]
+name_lst = []
+with open(fullname, 'r') as infile:
+    lname = input('Please enter a surname:')
+    for line in infile:
+        if lname in line or lname.lower() in line.lower() or lname.upper() in line.upper():
+            name_lst.append(line.split(' ')[0])
+if len(name_lst) > 0:
+    print(name_lst)
+else:
+    print('No-one has that surname')
+def get_surname(fullname):
+    return fullname.split(' ')[1]
+def get_firstname(fullname):
+    return fullname.split(' ')[0]
+
